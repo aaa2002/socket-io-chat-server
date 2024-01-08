5a12be834543223ec1ee033981a2330f9064d5bf
aaa2002 <alexand2002@gmail.com>
2024-01-08 17:43:00 +0200

    cors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const cors = require('cors'); // Import the cors middleware
 
-const server = http.createServer();
+const app = express();
+const server = http.createServer(app);
 const io = socketIO(server);
+
+// Use CORS middleware
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -10,7 +20,7 @@ io.on('connection', (socket) => {
   // Listen for messages from the client
   socket.on('chat message', (msg) => {
     console.log(`Message: ${msg}`);
-
+    
     // Broadcast the message to all connected clients
     io.emit('chat message', msg);
   });
