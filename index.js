4f7ff7aa2e6eb8091d1d78caed52204950c53808
aaa2002 <alexand2002@gmail.com>
2024-01-08 17:23:50 +0200

    commit 1

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const socketIO = require('socket.io');
+
+const server = http.createServer();
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for messages from the client
+  socket.on('chat message', (msg) => {
+    console.log(`Message: ${msg}`);
+
+    // Broadcast the message to all connected clients
+    io.emit('chat message', msg);
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
